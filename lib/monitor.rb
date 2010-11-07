91f74f6f42acf71dfc2f13205683641244112885
Soul Rebel <set.krag@gmail.com>
2010-11-07 11:33:11 +0100

    few new methods

--- lib/monitor.rb
+++ lib/monitor.rb
@@ -1,3 +1,30 @@
+# Module containing all monitor classes.
+#
+# == Info
+# * *Package*    Monitor
+#
 module Monitor
     
+  # Main monitor abstract class.
+  # All other monitors extend it.
+  #
+  # == Info
+  # * *Package*    Monitor
+  #
+  class Base
+    def initialize
+      
+    end
+  end
+
+  def decode_size(bytes)
+    types = ['B', 'KiB', 'MiB', 'GiB', 'TiB']
+    i = 0
+    while bytes >= 1024 && i < (types.length) -1
+      bytes /= 1024
+      i += 1
+    end
+
+    return ("%.02f" % bytes).to_s + " " + types[i].to_s
+  end
 end
