87e1d6271605c5d2c352dd3f81ef66ad2ca5b863
Soul Rebel <set.krag@gmail.com>
2011-03-12 20:53:28 +0100

    fixing some issues with class constants.. some still unresolved..

--- lib/monitor_cpu.rb
+++ lib/monitor_cpu.rb
@@ -2,8 +2,10 @@ require 'sys_monitor'
 
 class MonitorCpu < SysMonitor::Base
   CPU_FILE = '/proc/loadavg'
+  CHECKS = ['ONE', 'FIVE', 'FIFTEEN']
   LOAD_AVG_ONE_MIN = 0
   LOAD_AVG_FIVE_MIN = 1
+  LOAD_AVG_FIFTEEN_MIN = 2
 
   include SysMonitor::Misc
   def initialize
@@ -11,14 +13,13 @@ class MonitorCpu < SysMonitor::Base
   end
 
   def do_monitoring
-    load = IO.read(MonitorCpu::CPU_FILE)
+    load = IO.read(CPU_FILE)
     load_avg = load.split(' ')
-    @log.info "Load 1 min: #{load_avg[self::LOAD_AVG_ONE_MIN]}"
-    if load_avg[self::LOAD_AVG_ONE_MIN] > @config[:cpu][:limit][:one_min]
-      
-    end
-    if load_avg[self::LOAD_AVG_FIVE_MIN] > @config[:cpu][:limit][:five_min]
+    CHECKS.each do |check|
+      @log.info "Load #{check} min: " + load_avg["#{self.class}::LOAD_AVG_#{check}_MIN".constantize]
+      if load_avg["LOAD_AVG_#{check}_MIN".constantize].to_i > @config[:cpu][:limit]["#{check.downcase}_min".to_sym]
 
+      end
     end
   end
 end
